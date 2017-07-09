37cde727277c354b9b9979c8cba4052aca9e923d
Kristen Sheppard <kristensheppard07@gmail.com>
2017-07-09 19:03:35 -0400

    outlined steps on how to continue process in comments

--- hangman.py
+++ hangman.py
@@ -15,8 +15,24 @@ def word_picker(word_list):
 		words_used.append(choice)
 		return choice
 
+#user inputs letter guesses
+#limit on guesses
+#grab a word for guessing
+#check if user input a single letter
+#is letter in hidden work? If so, how many times does it appear?
+#print the ltters
+#add +1 to counter variable if guess is wrong to limit the guesses
 
-
+# Concepts to keep in mind:
+# Random
+# Variables
+# Boolean
+# Input and Output
+# Integer
+# Char
+# String
+# Length
+# Print
 
 
 
